e33be1a12f84803788defab1dd1abddc3ec40289
joehe <joehe@chancetop.com>
2021-12-02 17:54:58 +0800

    2021-12-2

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,5 @@
+/**
+ * @author joe
+ */
+class Day1 {
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
+import java.io.File
+
 /**
  * @author joe
  */
 fun main() {
-
+    val file = File(ClassLoader.getSystemResource("day1.txt").toURI())
+    file.forEachLine { println(it) }
 }
